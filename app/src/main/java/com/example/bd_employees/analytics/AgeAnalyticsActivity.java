dd67ec2b8d1e2f092842dbb921e25b8f88df3212
maxim <maximdeadline@gmail.com>
2020-04-28 15:08:37 +0300

    Сверстан экран аналитики, добавлены интенты на графики

---
+++ app/src/main/java/com/example/bd_employees/analytics/AgeAnalyticsActivity.java
@@ -0,0 +1,16 @@
+package com.example.bd_employees.analytics;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.bd_employees.R;
+
+public class AgeAnalyticsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_age_analytics);
+    }
+}
--- app/src/main/java/com/example/bd_employees/fragments/AnalyticsFragment.java
+++ app/src/main/java/com/example/bd_employees/fragments/AnalyticsFragment.java
@@ -1,15 +1,56 @@
 package com.example.bd_employees.fragments;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-import com.example.bd_employees.R;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.cardview.widget.CardView;
+import androidx.fragment.app.Fragment;
+
+import com.example.bd_employees.R;
+import com.example.bd_employees.analytics.AgeAnalyticsActivity;
+import com.example.bd_employees.analytics.EducationAnalyticsActivity;
+import com.example.bd_employees.analytics.SalaryAnalyticsActivity;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
 public class AnalyticsFragment extends Fragment {
+
 
     public AnalyticsFragment() {
         // Required empty public constructor
     }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_analytics, container, false);
+
+        CardView cardView1 = view.findViewById(R.id.cardView1);
+        cardView1.setOnClickListener(v -> {
+            Intent intent = new Intent(AnalyticsFragment.this.getActivity(), EducationAnalyticsActivity.class);
+            startActivity(intent);
+        });
+
+        CardView cardView2 = view.findViewById(R.id.cardView2);
+        cardView2.setOnClickListener(v -> {
+            Intent intent = new Intent(AnalyticsFragment.this.getActivity(), SalaryAnalyticsActivity.class);
+            startActivity(intent);
+        });
+
+        CardView cardView3 = view.findViewById(R.id.cardView3);
+        cardView3.setOnClickListener(v -> {
+            Intent intent = new Intent(AnalyticsFragment.this.getActivity(), AgeAnalyticsActivity.class);
+            startActivity(intent);
+        });
+
+        return view;
+    }
+
 }
